1844890fca28113c92861661f1ef3d123eca01ec
David Grayson <davidegrayson@gmail.com>
2012-08-30 09:02:27 -0700

    Fixed the format of the raw output.  It needs to be compatible with spreadsheet programs and the imu-calibrator.py script.

--- minimu9-ahrs.cpp
+++ minimu9-ahrs.cpp
@@ -28,7 +28,7 @@ void streamRawValues(IMU& imu)
     while(1)
     {
         imu.read();
-        printf("m: %7d %7d %7d  a: %7d %7d %7d  g: %7d %7d %7d\n",
+        printf("%7d %7d %7d  %7d %7d %7d  %7d %7d %7d\n",
                imu.m[0], imu.m[1], imu.m[2],
                imu.a[0], imu.a[1], imu.a[2],
                imu.g[0], imu.g[1], imu.g[2]
