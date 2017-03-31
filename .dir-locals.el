d416754f66f00f2e7830ec4961aa9cfa1578d97d
David Grayson <davidegrayson@gmail.com>
2017-03-31 17:59:21 +0000

    Set the fill-column to 80 in .dir-locals.el.

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,4 +3,5 @@
 (
   (c-mode . ((c-basic-offset . 2) (tab-width . 2) (indent-tabs-mode . nil) (c-file-style . "linux")) )
   (c++-mode . ((c-basic-offset . 2) (tab-width . 2) (indent-tabs-mode . nil) (c-file-style . "linux")) )
+  (nil . ((fill-column . 80)))
 )
