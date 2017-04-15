e8f6b27f88bff005f7d1944708174e26c15f1da0
David Grayson <davidegrayson@gmail.com>
2017-04-15 20:31:33 +0000

    Draw a line in imu.h.

--- imu.h
+++ imu.h
@@ -5,10 +5,23 @@
 class imu
 {
 public:
-  virtual void read_raw() { read_mag_raw(); read_acc_raw(); read_gyro_raw(); }
+  virtual void read_raw()
+  {
+    read_mag_raw();
+    read_acc_raw();
+    read_gyro_raw();
+  }
+
   virtual void read_acc_raw() = 0;
   virtual void read_mag_raw() = 0;
   virtual void read_gyro_raw() = 0;
+
+  int32_t m[3];
+  int32_t a[3];
+  int32_t g[3];
+
+
+  // TODO: remove stuff below this point
 
   // Scaled readings
   virtual vector read_mag() = 0;  // In body coords, scaled to -1..1 range
@@ -22,8 +35,4 @@ public:
 
   vector gyro_offset;
   int_vector mag_min, mag_max;
-
-  int32_t m[3];
-  int32_t a[3];
-  int32_t g[3];
 };
